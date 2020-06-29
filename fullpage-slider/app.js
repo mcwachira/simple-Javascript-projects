f4c2b88cb50fef5234b5d7d726525dd5a3f13a2e
mcwachira <mcwachira@users.noreply.github.com>
2020-06-29 23:44:59 +0300

    first commit

---
+++ fullpage-slider/app.js
@@ -0,0 +1,31 @@
+confereence
+let slides = document.getElementsByClassName('images-style')
+
+let slider = 1
+function moveNext(slider )  {
+if( slider > slides.length)  {
+slider = 1
+}
+for(let i =0; i<slides.length;i++)   {
+    slides[i].style.display = 'none'
+}
+slides[slider- 1].style.display = 'block'
+
+}
+
+function movePrev() {
+    if(slider < slider.length)  {
+        slider = slider.length;
+        
+    }
+    slider--;
+}
+
+let next = document.querySelector('carousel__photo--next')
+  if(next)  {
+      next.addEventListener('click'. moveNext)
+  }
+  let prev = document.querySelector('carousel__photo--prev')
+  if(prev)  {
+      prev.addEventListener('click'. movePrev)
+  }
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Simple Javascript Projects
+this is a project that made up  simple javascript projects like a carousel, digital/analogue clock, a calculator etc.
+The project includes the html and css files.
+
+# Practice
+The  projects are written in pure Javascript with no frameworks included.The project is to help you better your skills in vanilla Javascript.
+
+## Contributing
+Pull requests and additional simple Js projects are welcomed.

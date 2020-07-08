c8e22abb8aecbb092d098ef09eb29082a0937a6d
mcwachira <mcwachira@users.noreply.github.com>
2020-07-08 15:28:32 +0300

    added some animatins to the responsive navigation menu

--- responsive-navigation-menu/app.js
+++ responsive-navigation-menu/app.js
@@ -1,6 +1,9 @@
 let menuButton = document.getElementById('js-navbar');
-let menu = document.getElementById('js-menu');
+let menu =  document.getElementById('js-menu');
+let list = document.querySelector('.main-nav li')
 
 menuButton.addEventListener('click', function() {
     menu.classList.toggle('active')
+    menuButton.classList.toggle('toggle')
+    
 })

fbb316885956853eec9c636b5e52b89050a2dea9
mcwachira <mcwachira@users.noreply.github.com>
2020-07-11 09:20:52 +0300

    made small changes to the digitLal clock project

--- digital-clock/app.js
+++ digital-clock/app.js
@@ -10,7 +10,7 @@ function displayTime()  {
     if(minutes < 10)    {
         minutes = "0" + minutes
     }
-    if(seconds < 1)    {
+    if(seconds < 10)    {
         seconds = "0" + seconds
     }
 

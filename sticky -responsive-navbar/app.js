957dd4f3586a80d7f6a28aaab2cbf495583af33b
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-04-19 15:16:30 +0300

    added a drop-down menu to the navigation

--- sticky -responsive-navbar/app.js
+++ sticky -responsive-navbar/app.js
@@ -10,6 +10,13 @@ const menu = document.querySelector('.menu')
 const openMenu = document.querySelector('.menu-btn')
 const closeMenu = document.querySelector('.close-btn')
 
+
+
+const dropDown = document.querySelector('.dropdown')
+const dropdownMenu = document.querySelector('.dropdown-menu')
+
+
+
 openMenu.addEventListener('click', () => {
     menu.classList.add('active')
 })
@@ -17,4 +24,8 @@ openMenu.addEventListener('click', () => {
 closeMenu.addEventListener('click', () => {
     // header.classList.remove('sticky')
     menu.classList.remove('active')
+})
+
+dropDown.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('display')
 })
